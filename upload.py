516493ac472b58eb502767a42094a245964abcb8
TuanVu99 <vuluu99pro@gmail.com>
2024-04-02 17:39:54 +0700

    fix

--- upload.py
+++ upload.py
@@ -55,9 +55,9 @@ def update_chi_tieu(file, code, kenh, dong_sp):
         data = []
         if file in ['/opt/shared/BU1/KA/BU1_KA_2023.xlsx' , '/opt/shared/BU2/KA/BU2_KA_2023.xlsx']:
             columns =list(range(20))
-            print(file)
         else:
             columns = list(range(14))
+            print(file)
         for row in df.iterrows():
             row = list(row[1])
             if pd.isnull(row[0]):
